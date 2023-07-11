5d23aa4636cc982d0dceba45f56e516ce79b6c35
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-07-11 19:30:09 +0300

    make Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
+  return (
+    <div>
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
