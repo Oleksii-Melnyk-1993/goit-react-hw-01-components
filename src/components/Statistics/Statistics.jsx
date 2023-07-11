baae2a6c2bbc7cc517d6a44910651d284ce1a2b4
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-07-11 20:12:39 +0300

    shit

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,15 +4,15 @@
 export const Statistics = ({ title, stats }) => {
   return (
     <section class="statistics">
-      {title && <h2 class="title">Upload stats</h2>}
+      {title && <h2 class="title">{title}</h2>}
 
       <ul class="stat-list">
-        {stats.map(item => {
+        {stats.map(item => (
           <li key={item.id} class="item">
             <span class="label">{item.label}</span>
             <span class="percentage">{item.percentage}%</span>
-          </li>;
-        })}
+          </li>
+        ))}
       </ul>
     </section>
   );
